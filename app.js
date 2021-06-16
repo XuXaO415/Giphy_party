c2fc678ff8965e2a201716f5d940a757cadb38a8
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-06-16 15:15:54 -0700

    Added practice SpaceX exercise

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// function getData () {
-
 async function addGiphy () {
-  $ ('form').on ('submit', async function (e) {
-    e.preventDefault ();
-    let searchTerm = $searchInput.val ();
-    $searchInput.val ('');
+  const response = await axios.get ('http://api.giphy.com/v1/gifs/search');
+  const {results} = res.data.length;
 
-    //await and returns promise(s); get endpoint
-    //https://developers.giphy.com/docs/api/endpoint
-    const gif_search = await axios.get ('api.giphy.com/v1/gifs/search', {
-      params: {
-        q: searchTerm,
-        api_key: 'VaVgtOjzpC7036El6H6nzobBSVcG2u34',
-      },
-    });
-  });
-  //console.log ('This line is after axios.get');
-  //console.log ("Let's get this party started!");
+  let random = Math.floor (Math.random () * results);
 }
+console.log ('I happen after addGiphy()');
+//await and returns promise(s); get endpoint
+//https://developers.giphy.com/docs/api/endpoint
+// const gif_search = await axios.get ('http://api.giphy.com/v1/gifs/search', {
+//   params: {
+//     q: 'searchTerm',
+//     api_key: 'VaVgtOjzpC7036El6H6nzobBSVcG2u34',
+//   },
+// });
+//console.log (search.cheeseburgers);
+
+//console.log ('This line is after axios.get');
+//console.log ("Let's get this party started!");

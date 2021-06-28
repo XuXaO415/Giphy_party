51a1f4a2a737dba1df1f6eda81759297516bb143
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-06-27 19:02:58 -0700

    Made some changes to this assignment

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-async function addGiphy () {
-  const response = await axios.get ('http://api.giphy.com/v1/gifs/search');
-  const {results} = res.data.length;
+const $gifArea = $('#gif_area');
+const $searchInput = $('#submit_search');
 
-  let random = Math.floor (Math.random () * results);
-}
-console.log ('I happen after addGiphy()');
+
+
+function addGiphy(results) {
+    //const response = await axios.get('http://api.giphy.com/v1/gifs/search');
+    //const results = results.data.length;
+
+    let random = Math.floor(Math.random() * results);
+    if (random) {
+        let $divCol;
+        $divCol = $('<div>');
+        let $newGif;
+        $newGif = $('<img>');
+        src: results.data[random].image.orignal.url,
+
+            $divCol.append($newGif);
+        $gifArea.append($divCol);
+    }
+
+
+    $('form').on('submit', async function (e) {
+        e.preventDefault();
+
+        // let seachTerm = $searchInput.val();
+        // $searchInput.val('');
+        // const response = await axios.get('https://api.giphy.com/v1/gifs/search'
+        const gif_search = await axios.get('http://api.giphy.com/v1/gifs/search', {
+            params: {
+                q: 'searchTerm',
+                api_key: 'VaVgtOjzpC7036El6H6nzobBSVcG2u34'
+            },
+        });
+
+        addGiphy(gif_search.data);
+    })
+
+    $('remove').on('click', function () {
+        $gifArea.empty();
+    });
+
 //await and returns promise(s); get endpoint
 //https://developers.giphy.com/docs/api/endpoint
-// const gif_search = await axios.get ('http://api.giphy.com/v1/gifs/search', {
-//   params: {
-//     q: 'searchTerm',
-//     api_key: 'VaVgtOjzpC7036El6H6nzobBSVcG2u34',
-//   },
-// });
+
 //console.log (search.cheeseburgers);
 
 //console.log ('This line is after axios.get');
